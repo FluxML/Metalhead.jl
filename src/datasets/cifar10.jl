8551d0d6c7c8144083f3b293c872061c565b0330
Avik Pal <avikpal@iitk.ac.in>
2018-05-02 19:16:52 +0530

    Minor style fixes

--- src/datasets/cifar10.jl
+++ src/datasets/cifar10.jl
@@ -44,10 +44,10 @@ function Base.getindex(v::ValData{BinPackedFS}, i::Integer)
 end
 
 function Base.getindex(v::TrainData{BinPackedFS}, i::Integer)
-    batch = i÷10000
-    batch = i%10000==0?batch-1:batch
-    num = i%10000
-    num = num==0?i-10000*batch:num
+    batch = i ÷ 10000
+    batch = (i % 10000 == 0)? (batch - 1) : batch
+    num = i % 10000
+    num = (num == 0)? (i - 10000 * batch) : num
     file = "data_batch_$(batch+1).bin"
     TrainingImage(DataSet, i, open(joinpath(v.set.folder, file)) do f
         seek(f, (num-1)*3073)
