9dd50d0e0f7865edcc92549f9818401399fc8961
Elliot Saba <staticfloat@gmail.com>
2018-09-19 11:49:04 +0200

    Refactor tests a bit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,65 +1,43 @@
 using Metalhead, Test
 
-vgg19 = VGG19()
+# Standardized testing for the models of tomorrow
+@testset "Basic Model Tests" begin
+    for (T, MODEL) in [
+            (Float32, VGG19),
+            (Float32, SqueezeNet),
+            (Float64, DenseNet),
+            (Float64, GoogleNet),
+        ]
+        model = MODEL()
 
-testx = rand(Float32, 224, 224, 3, 1)
+        x_test = rand(T, 224, 224, 3, 1)
+        y_test = model(x_test)
 
-testy = vgg19(testx)
+        # Test that types and shapes work out as we expect
+        @test y_test isa AbstractArray
+        @test length(y_test) == 1000
 
-@test testy isa AbstractArray
-@test length(testy) == 1000
-
-squeezenet = SqueezeNet()
-
-testx = rand(Float32, 224, 224, 3, 1)
-
-testy = squeezenet(testx)
-
-@test testy isa AbstractArray
-@test length(testy) == 1000
-
-densenet = DenseNet()
-
-testx = rand(224, 224, 3, 1)
-
-testy = densenet(testx)
-
-@test testy isa AbstractArray
-@test length(testy) == 1000
-
-resnet = ResNet()
-
-testx = rand(224, 224, 3, 1)
-
-testy = resnet(testx)
-
-@test testy isa AbstractArray
-@test length(testy) == 1000
-
-googlenet = GoogleNet()
-
-testx = rand(224, 224, 3, 1)
-
-testy = googlenet(testx)
-
-@test testy isa AbstractArray
-@test length(testy) == 1000
-
-
-# Test that the models can be indexed
-
-@test length(vgg19.layers[1:4].layers) == 4
-@test length(squeezenet.layers[1:4].layers) == 4
-@test length(resnet.layers[1:4].layers) == 4
-@test length(googlenet.layers[1:4].layers) == 4
-@test length(densenet.layers[1:4].layers) == 4
+        # Test that the models can be indexed
+        @test length(model.layers[1:4].layers) == 4
+    end
+end
 
 # Test proper download and functioning of CIFAR10
-Metalhead.download(CIFAR10)
-x1 = trainimgs(CIFAR10)[1]
-x2 = valimgs(CIFAR10)[1]
-@test size(x1.img) == (32, 32)
-@test size(x2.img) == (32, 32)
+@testset "CIFAR dataset tests" begin
+    x1 = trainimgs(CIFAR10)[1]
+    x2 = valimgs(CIFAR10)[1]
+
+    # Test that the input is roughly what we expect
+    @test size(x1.img) == (32, 32)
+    @test size(x2.img) == (32, 32)
+end
+
+# Test printing of prediction
+@testset "Prediction table display" begin
+    x = valimgs(CIFAR10)[1]
+    m = VGG19()
+    predict(m, x)
+end
 
 # Just run the prediction code end-to-end
 # TODO: Set up travis to actually run these
