c8894ce58267dfd11fdb830f2c83a7735f0cda7d
Kyle Daruwalla <daruwalla@wisc.edu>
2021-02-22 13:29:46 -0600

    Fix typos in googlenet

--- src/googlenet.jl
+++ src/googlenet.jl
@@ -1,24 +1,24 @@
 function inceptionblock(inplanes, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, pool_proj)
-    branch1 = Chain(Conv((1, 1), inplanes => out_1x1)...)
+    branch1 = Chain(Conv((1, 1), inplanes => out_1x1))
   
-    branch2 = Chain(Conv((1, 1), inplanes => red_3x3)...,
-                    Conv((3, 3), red_3x3 => out_3x3; pad=1)...)        
+    branch2 = Chain(Conv((1, 1), inplanes => red_3x3),
+                    Conv((3, 3), red_3x3 => out_3x3; pad=1))        
   
-    branch3 = Chain(Conv((1, 1), inplanes => red_5x5)...,
-                    Conv((5, 5), red_5x5 => out_5x5; pad=2)...) 
+    branch3 = Chain(Conv((1, 1), inplanes => red_5x5),
+                    Conv((5, 5), red_5x5 => out_5x5; pad=2)) 
   
     branch4 = Chain(MaxPool((3, 3), stride=1, pad=1),
-                    Conv((1, 1), inplanes => pool_proj)...)
+                    Conv((1, 1), inplanes => pool_proj))
   
     return Parallel(cat_channels,
                     branch1, branch2, branch3, branch4)
 end
   
 function googlenet()
-  layers = Chain(Conv((7, 7), 3 => 64; stride=2, pad=3)...,
+  layers = Chain(Conv((7, 7), 3 => 64; stride=2, pad=3),
                  MaxPool((3, 3), stride=2, pad=1),
-                 Conv((1, 1), 64 => 64)...,
-                 Conv((3, 3), 64 => 192; pad=1)...,
+                 Conv((1, 1), 64 => 64),
+                 Conv((3, 3), 64 => 192; pad=1),
                  MaxPool((3, 3), stride=2, pad=1),
                  inceptionblock(192, 64, 96, 128, 16, 32, 32),
                  inceptionblock(256, 128, 128, 192, 32, 96, 64),
