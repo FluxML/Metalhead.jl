37538af5060e5c0e4bea063bb8fae032d09e809f
Kyle Daruwalla <daruwalla@wisc.edu>
2021-07-29 09:20:38 -0500

    Add gradient tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,22 @@
 using Metalhead, Test
+using Flux
+using Flux: Zygote
 
 PRETRAINED_MODELS = [(VGG19, false), ResNet50, GoogLeNet, DenseNet121, SqueezeNet]
+
+function gradtest(model, input)
+  y, pb = Zygote.pullback(() -> model(input), Flux.params(model))
+  gs = pb(ones(Float32, size(y)))
+
+  # if we make it to here with no error, success!
+  return true
+end
 
 @testset "AlexNet" begin
   model = AlexNet()
   @test size(model(rand(Float32, 256, 256, 3, 50))) == (1000, 50)
   @test_throws ArgumentError AlexNet(pretrain = true)
+  @test gradtest(model, rand(Float32, 256, 256, 3, 50))
 end
 
 @testset "VGG" begin
@@ -19,19 +30,21 @@ end
     else
       @test_throws ArgumentError model(batchnorm = bn, pretrain = true)
     end
+    @test gradtest(m, rand(Float32, imsize..., 3, 50))
   end
 end
 
 @testset "ResNet" begin
   @testset for model in [ResNet18, ResNet34, ResNet50, ResNet101, ResNet152]
     m = model()
+    
     @test size(m(rand(Float32, 256, 256, 3, 50))) == (1000, 50)
-
     if model in PRETRAINED_MODELS
       @test (model(pretrain = true); true)
     else
       @test_throws ArgumentError model(pretrain = true)
     end
+    @test gradtest(m, rand(Float32, 256, 256, 3, 50))
   end
 end
 
@@ -39,29 +52,33 @@ end
   m = GoogLeNet()
   @test size(m(rand(Float32, 224, 224, 3, 50))) == (1000, 50)
   @test (GoogLeNet(pretrain = true); true)
+  @test gradtest(m, rand(Float32, 224, 224, 3, 50))
 end
 
 @testset "Inception3" begin
   m = Inception3()
   @test size(m(rand(Float32, 299, 299, 3, 50))) == (1000, 50)
   @test_throws ArgumentError Inception3(pretrain = true)
+  @test gradtest(m, rand(Float32, 299, 299, 3, 50))
 end
 
 @testset "SqueezeNet" begin
   m = SqueezeNet()
   @test size(m(rand(Float32, 227, 227, 3, 50))) == (1000, 50)
   @test (SqueezeNet(pretrain = true); true)
+  @test gradtest(m, rand(Float32, 227, 227, 3, 50))
 end
 
 @testset "DenseNet" begin
   @testset for model in [DenseNet121, DenseNet161, DenseNet169, DenseNet201]
     m = model()
-    @test size(m(rand(Float32, 224, 224, 3, 50))) == (1000, 50)
 
+    @test size(m(rand(Float32, 224, 224, 3, 50))) == (1000, 50)
     if model in PRETRAINED_MODELS
       @test (model(pretrain = true); true)
     else
       @test_throws ArgumentError model(pretrain = true)
     end
+    @test gradtest(m, rand(Float32, 224, 224, 3, 50))
   end
 end
