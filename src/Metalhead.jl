b8e45527b8e9ea7158ef70e774f72da04b2bd2d1
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2022-01-16 13:19:00 +0530

    Update Metalhead.jl

--- src/Metalhead.jl
+++ src/Metalhead.jl
@@ -23,7 +23,7 @@ export  AlexNet,
         VGG, VGG11, VGG13, VGG16, VGG19,
         ResNet, ResNet18, ResNet34, ResNet50, ResNet101, ResNet152,
         GoogLeNet, Inception3, SqueezeNet,
-        DenseNet, DenseNet121, DenseNet161, DenseNet169, DenseNet201
+        DenseNet, DenseNet121, DenseNet161, DenseNet169, DenseNet201,
         ResNeXt
 
 # use Flux._big_show to pretty print large models
