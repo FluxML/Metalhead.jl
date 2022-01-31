d7933b1dc5f002f154042ff1e154d2eb858c9c50
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2022-01-31 09:18:46 +0530

    Clean up files

--- test/vit-like.jl
+++
@@ -1,7 +0,0 @@
-using Metalhead, Test
-using Flux
-
-@testset "MLPMixer" begin
-    @test size(MLPMixer()(rand(Float32, 256, 256, 3, 67))) == (1000, 67)
-    @test_skip gradtest(MLPMixer(), rand(Float32, 256, 256, 3, 67))
-end
