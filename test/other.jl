d589f42c931172d87774bdac1d80d12cafc1745f
Rutvij Wamanse <wamanserutvij25@gmail.com>
2022-02-15 16:13:41 +0530

    added tests

--- test/other.jl
+++ test/other.jl
@@ -8,7 +8,7 @@ using Flux
 end
 
 @testset "ESRGAN" begin
-    D, G = Metalhead.ESRGAN()
+    D, G = ESRGAN()
     @test size(G(rand(Float32,24,24,3,5))) == (96, 96, 3, 5)
     @test size(D(G(rand(Float32,24,24,3,5)))) == (1,5)
 end
