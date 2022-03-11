52240d30ffa485f0d72b2de9fb5004d190d19683
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2022-03-11 16:43:49 +0530

    Added tests

--- test/convnets.jl
+++ test/convnets.jl
@@ -126,6 +126,8 @@ end
   end
 end
 
+GC.gc()
+
 @testset "ConvNeXt" verbose = true begin
   @testset for mode in [:tiny, :small, :base, :large, :xlarge]
     @testset for drop_path_rate in [0.0, 0.5, 0.99]
@@ -134,5 +136,16 @@ end
       @test size(m(rand(Float32, 224, 224, 3, 2))) == (1000, 2)
       @test_skip gradtest(m, rand(Float32, 224, 224, 3, 2))
     end
+  end
+end
+
+GC.gc()
+
+@testset "ConvMixer" verbose = true begin
+  @testset for mode in [:base, :large, :small]
+    m = ConvMixer(mode)
+
+    @test size(m(rand(Float32, 224, 224, 3, 2))) == (1000, 2)
+    @test_skip gradtest(m, rand(Float32, 224, 224, 3, 2))
   end
 end
