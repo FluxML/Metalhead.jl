d51e3a4914ea80c2c54e521ec8a5cd2d88108c39
Kyle Daruwalla <daruwalla@wisc.edu>
2022-03-31 20:44:18 -0500

    Enable some ViT tests

--- test/vit-based.jl
+++ test/vit-based.jl
@@ -2,7 +2,7 @@ using Metalhead, Test
 using Flux
 
 @testset "ViT" begin
-  for mode in [:tiny, :small, :base, :large, :huge, :giant, :gigantic]
+  for mode in [:tiny, :small, :base, :large] #,:huge, :giant, :gigantic]
     m = ViT(mode)
     @test size(m(rand(Float32, 256, 256, 3, 2))) == (1000, 2)
     @test_skip gradtest(m, rand(Float32, 256, 256, 3, 2))
