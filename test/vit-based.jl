6550c995dfbfe4491fd29e65754a39ebf0a4c77e
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-04 17:13:20 -0500

    Revert ViT spatial dims

--- test/vit-based.jl
+++ test/vit-based.jl
@@ -4,8 +4,8 @@ using Flux
 @testset "ViT" begin
   for mode in [:tiny, :small, :base, :large] #,:huge, :giant, :gigantic]
     m = ViT(mode)
-    @test size(m(rand(Float32, 224, 224, 3, 1))) == (1000, 1)
-    @test_skip gradtest(m, rand(Float32, 224, 224, 3, 1))
+    @test size(m(rand(Float32, 256, 256, 3, 1))) == (1000, 1)
+    @test_skip gradtest(m, rand(Float32, 256, 256, 3, 1))
   end
   GC.gc()
 end
