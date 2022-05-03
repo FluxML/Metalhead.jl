12e670c7ddb4d8b1d6eb0134429946bc702af75e
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2022-05-03 08:27:16 +0530

    Larger tests first

--- test/vit-based.jl
+++ test/vit-based.jl
@@ -2,7 +2,7 @@ using Metalhead, Test
 using Flux
 
 @testset "ViT" begin
-  for mode in [:tiny, :small, :base, :large] #,:huge, :giant, :gigantic]
+  for mode in [:large, :base, :small] # :tiny, #,:huge, :giant, :gigantic]
     m = ViT(mode)
     @test size(m(x_256)) == (1000, 1)
     @test gradtest(m, x_256)
