a31cfec4c560e35824d0dc0c0c7a4d23a8eb7bf9
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2022-05-04 07:45:38 +0530

    Revert

--- test/vit-based.jl
+++ test/vit-based.jl
@@ -2,7 +2,7 @@ using Metalhead, Test
 using Flux
 
 @testset "ViT" begin
-  for mode in [:large, :base, :small] # :tiny, #,:huge, :giant, :gigantic]
+  for mode in [:small, :base, :large] # :tiny, #,:huge, :giant, :gigantic]
     m = ViT(mode)
     @test size(m(x_256)) == (1000, 1)
     @test gradtest(m, x_256)
