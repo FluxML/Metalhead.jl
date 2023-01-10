99f07ad919e70efa3897158cfc35c1d27e792fd2
Anshuman Mishra <51750587+shivance@users.noreply.github.com>
2023-01-10 14:34:58 +0530

    adapting to fastai's unet impl

--- src/convnets/unet.jl
+++ src/convnets/unet.jl
@@ -1,73 +1,97 @@
-function unet_block(in_chs::Int, out_chs::Int, kernel = (3, 3))
-	return Chain(conv1 = Conv(kernel, in_chs => out_chs; pad = (1, 1)),
-		norm1 = BatchNorm(out_chs, relu),
-		conv2 = Conv(kernel, out_chs => out_chs; pad = (1, 1)),
-		norm2 = BatchNorm(out_chs, relu))
+function PixelShuffleICNR(inplanes, outplanes; r = 2)
+    return Chain(basic_conv_bn((1, 1), inplanes, outplanes * (r^2)),
+                 Flux.PixelShuffle(r))
 end
 
-function upconv_block(in_chs::Int, out_chs::Int, kernel = (2, 2))
-	return ConvTranspose(kernel, in_chs => out_chs; stride = (2, 2))
+function UNetCombineLayer(inplanes, outplanes)
+    return Chain(basic_conv_bn((3, 3), inplanes, outplanes; pad = 1),
+                 basic_conv_bn((3, 3), outplanes, outplanes; pad = 1))
 end
 
-function unet(in_channels::Integer = 3, out_channels::Integer = in_channels,
-	features::Integer = 32)
-	encoder_conv = []
-	push!(encoder_conv,
-		unet_block(in_channels, features))
+function UNetMiddleBlock(inplanes)
+    return Chain(basic_conv_bn((3, 3), inplanes, 2inplanes; pad = 1),
+                 basic_conv_bn((3, 3), 2inplanes, inplanes; pad = 1))
+end
 
-	append!(encoder_conv,
-		[unet_block(features * 2^i, features * 2^(i + 1)) for i in 0:2])
+function UNetFinalBlock(inplanes, outplanes)
+    return Chain(basicblock(inplanes, inplanes; reduction_factor = 1),
+                 basic_conv_bn((1, 1), inplanes, outplanes))
+end
 
-	encoder_conv = Chain(encoder_conv)
-	encoder_pool = [Chain(encoder_conv[i], MaxPool((2, 2); stride = (2, 2))) for i in 1:4]
+function unetlayers(layers, sz; outplanes = nothing, skip_upscale = 0,
+                    m_middle = _ -> (identity,))
+    isempty(layers) && return m_middle(sz[end - 1])
 
-	bottleneck = unet_block(features * 8, features * 16)
-	layers = Chain(encoder_conv, bottleneck)
+    layer, layers = layers[1], layers[2:end]
+    outsz = Flux.outputsize(layer, sz)
+    does_downscale = sz[1] ÷ 2 == outsz[1]
 
-	upconv = Chain([upconv_block(features * 2^(i + 1), features * 2^i)
-					for i in 0:3]...)
+    if !does_downscale
+        return Chain(layer, unetlayers(layers, outsz; outplanes, skip_upscale)...)
+    elseif does_downscale && skip_upscale > 0
+        return Chain(layer,
+                     unetlayers(layers, outsz; skip_upscale = skip_upscale - 1,
+                                outplanes)...)
+    else
+        childunet = Chain(unetlayers(layers, outsz; skip_upscale)...)
+        outsz = Flux.outputsize(childunet, outsz)
 
-	concat_layer = Chain([Parallel(cat_channels,
-		encoder_pool[i],
-		upconv[i])
-						  for i in 1:4]...)
+        inplanes = sz[end - 1]
+        midplanes = outsz[end - 1]
+        outplanes = isnothing(outplanes) ? inplanes : outplanes
 
-	decoder_layer = Chain([unet_block(features * 2^(i + 1), features * 2^i) for i in 3:-1:0]...)
+        return UNetBlock(Chain(layer, childunet),
+                         inplanes, midplanes, outplanes)
+    end
+end
 
-	layers = Chain(layers, decoder_layer)
-
-	decoder = Chain([Chain([
-		concat_layer[i],
-		decoder_layer[5-i]])
-					 for i in 4:-1:1]...)
-
-	final_conv = Conv((1, 1), features => out_channels, σ)
-
-	decoder = Chain(decoder, final_conv)
-
-	return layers
+function UNetBlock(m_child, inplanes, midplanes, outplanes = 2inplanes)
+    return Chain(;
+                 upsample = SkipConnection(Chain(; child = m_child,
+                                                 upsample = PixelShuffleICNR(midplanes,
+                                                                             midplanes)),
+                                           Parallel(cat_channels, identity,
+                                                    BatchNorm(inplanes))),
+                 act = xs -> relu.(xs),
+                 combine = UNetCombineLayer(inplanes + midplanes, outplanes))
 end
 
 """
-	UNet(in_channels::Integer = 3, inplanes::Integer = 32, outplanes::Integer = inplanes)
-	Create a UNet model
-	([reference](https://arxiv.org/abs/1505.04597v1))
-	# Arguments
-	- `in_channels`: The number of input channels
-	- `inplanes`: The number of input features to the network
-	- `outplanes`: The number of output features
+    UNet(backbone, inputsize)
+    DenseNet(transition_configs::NTuple{N,Integer})
+
+Creates a UNet model with specified backbone. Backbone of Any Metalhead model can be used as
+encoder .
+Set `pretrain = true` to load the model with pre-trained weights for ImageNet.
+
 !!! warning
-	`UNet` does not currently support pretrained weights.
+
+    `UNet` does not currently support pretrained weights.
+
+See also [`Metalhead.UNet`](@ref).
 """
 struct UNet
-	layers::Any
+    layers::Any
 end
 @functor UNet
 
-function UNet(in_channels::Integer = 3, inplanes::Integer = 32,
-	outplanes::Integer = inplanes)
-	layers = unet(in_channels, inplanes, outplanes)
-	return UNet(layers)
+function UNet(backbone,
+              inputsize,
+              outplanes,
+              final = UNetFinalBlock,
+              fdownscale::Integer = 0,
+              kwargs...)
+    backbonelayers = collect(iterlayers(backbone))
+    layers = unetlayers(backbonelayers,
+                        inputsize;
+                        m_middle = UNetMiddleBlock,
+                        skip_upscale = fdownscale,
+                        kwargs...)
+
+    outsz = Flux.outputsize(layers, inputsize)
+    layers = Chain(layers, final(outsz[end - 1], outplanes))
+
+    return UNet(layers)
 end
 
 (m::UNet)(x::AbstractArray) = m.layers(x)
