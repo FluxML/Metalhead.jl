fb990581783789444e9aa5da6afb0844e8a51f85
Brian Chen <ToucheSir@users.noreply.github.com>
2023-02-04 09:04:55 -0800

    WideResNet is broken too

--- test/convnets.jl
+++ test/convnets.jl
@@ -63,7 +63,7 @@ end
             @test gradtest(m, x_224)
             _gc()
             if (WideResNet, sz) in PRETRAINED_MODELS
-                @test acctest(WideResNet(sz, pretrain = true))
+                @test_broken acctest(WideResNet(sz, pretrain = true))
             else
                 @test_throws ArgumentError WideResNet(sz, pretrain = true)
             end
