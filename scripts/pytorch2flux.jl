658588fe2d40f29875491a97266d1f8f155bb97e
CarloLucibello <carlo.lucibello@gmail.com>
2023-04-24 07:46:51 +0200

    update

---
+++ scripts/pytorch2flux.jl
@@ -0,0 +1,127 @@
+# Converts the weigths of a PyTorch model to a Flux model from Metalhead
+# PyTorch need to be installed
+# Tested on ResNet and VGG models
+
+using Flux
+using Metalhead
+using DataStructures
+using Statistics, Random, LinearAlgebra
+using BSON
+using PythonCall
+using Images
+
+include("utils.jl")
+
+const torch = pyimport("torch")
+const torchvision = pyimport("torchvision")
+
+# test image
+const GUITAR_PATH = download("https://cdn.pixabay.com/photo/2015/05/07/11/02/guitar-756326_960_720.jpg")
+const IMAGENET_LABELS = readlines(download("https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"))
+
+function compare_pytorch(jlmodel, pymodel)
+    sz = (224, 224)
+    img = Images.load(GUITAR_PATH);
+    img = imresize(img, sz);
+    # CHW -> WHC
+    data = permutedims(convert(Array{Float32}, channelview(img)), (3,2,1))
+    data = imagenet_normalize(data[:,:,:,1:1])
+    
+    println("  Flux:")
+    Flux.testmode!(jlmodel)
+    out = jlmodel(data)
+    jlprobs = softmax(out)[:,1]
+    for i in sortperm(jlprobs, rev=true)[1:5]
+        println("    $(IMAGENET_LABELS[i]): $(jlprobs[i])")
+    end
+
+    println("  PyTorch:")
+    pymodel.eval()
+    out = pymodel(jl2th(data))
+    pyprobs = torch.nn.functional.softmax(out[0], dim=0).detach().numpy()
+    pyprobs = np2jl(pyprobs)
+    for i in sortperm(pyprobs, rev=true)[1:5]
+        println("    $(IMAGENET_LABELS[i]): $(pyprobs[i])")
+    end
+
+    @assert maximum(jlprobs) ≈ maximum(pyprobs)
+    @assert argmax(jlprobs) ≈ argmax(pyprobs)
+    @assert sortperm(jlprobs, rev=true)[1:10] ≈ sortperm(pyprobs, rev=true)[1:10] 
+    println()
+end
+
+function _list_state(node::BatchNorm, channel, prefix)
+    # use the same order of parameters than PyTorch
+    prefix = prefix * ".batchnorm_"
+    put!(channel, (prefix * "γ", node.γ)) # weigth (learnable)
+    put!(channel, (prefix * "β", node.β)) # bias (learnable)
+    put!(channel, (prefix * "μ", node.μ))  # running mean
+    put!(channel, (prefix * "σ²", node.σ²)) # running variance
+end
+
+function _list_state(node::Conv, channel, prefix)
+    prefix = prefix * ".conv_"
+    put!(channel, (prefix * "weight", node.weight))
+    if node.bias isa AbstractArray
+        put!(channel, (prefix * "bias", node.bias))
+    end
+end
+
+function _list_state(node::Dense, channel, prefix)
+    prefix = prefix * ".dense_"
+    put!(channel, (prefix * "weight", node.weight))
+    if node.bias isa AbstractArray
+        put!(channel, (prefix * "bias", node.bias))
+    end
+end
+
+_list_state(node, channel, prefix) = nothing
+
+function _list_state(node::Union{Chain,Parallel}, channel, prefix)
+    for (i, n) in enumerate(node.layers)
+        _list_state(n, channel, prefix * ".layers[$i]")
+    end
+end
+
+function list_state(node; prefix = "model")
+    Channel() do channel
+        _list_state(node, channel, prefix)
+    end
+end
+
+function pytorch2flux!(jlmodel, pymodel)
+    state = OrderedDict(list_state(jlmodel.layers))
+
+    state_dict = pymodel.state_dict()
+    # pytorch_pp = OrderedDict((k, v.numpy()) for (k, v) in state_dict if !occursin("num_batches_tracked", convert(Any,k)))
+    pytorch_pp = OrderedDict((py2jl(k), th2jl(v)) for (k, v) in state_dict.items())
+
+    # loop over all parameters
+    for ((flux_key, flux_param), (pytorch_key, pytorch_param)) in zip(state, pytorch_pp)
+        param_name = split(flux_key, ".")[end]
+        if param_name == "dense_weight"
+            flux_param .= permutedims(pytorch_param, (2,1))
+        elseif  param_name == "conv_weight"
+            flux_param .= reverse(pytorch_param, dims=(1, 2))
+        else
+            flux_param .= pytorch_param
+        end
+    end
+end
+
+
+## Compare pretrained model to PyTorch
+# jlmodel = Metalhead.VGG(11, pretrain=true)
+# pymodel = torchvision.models.vgg11(weights="IMAGENET1K_V1")
+# compare_pytorch(jlmodel, pymodel)
+
+
+## Create model, set weights from pytorch, save to BSON 
+# modelname = "VGG11"
+# weights = "IMAGENET1K_V1"
+# jlmodel = VGG(11)
+# pymodel = torchvision.models.vgg11(weights=weights)
+# pytorch2flux!(jlmodel, pymodel)
+# compare_pytorch(jlmodel, pymodel)
+# BSON.@save joinpath(@__DIR__,"$(modelname)_$weights.bson") model=jlmodel
+
