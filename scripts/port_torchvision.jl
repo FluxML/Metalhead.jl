ebc9f04fc1e6ece4d2b9e07b2af6a4b6721512c2
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2023-04-24 08:53:36 +0200

    add pytorch/torchvision porting and comparison script (#225)
    * compare script
    
    * move conda pkg
    
    * add docs
    
    * update
    
    * update

---
+++ scripts/port_torchvision.jl
@@ -0,0 +1,37 @@
+include("pytorch2flux.jl")
+
+const tvmodels = pyimport("torchvision.models")
+
+# name, weight, jlconstructor, pyconstructor
+model_list = [
+            #   ("vgg11", "IMAGENET1K_V1", () -> VGG(11), weights -> tvmodels.vgg11(weights=weights)),
+            #   ("vgg13", "IMAGENET1K_V1", () -> VGG(13), weights -> tvmodels.vgg13(weights=weights)),
+            #   ("vgg16", "IMAGENET1K_V1", () -> VGG(16), weights -> tvmodels.vgg16(weights=weights)),
+            #   ("vgg19", "IMAGENET1K_V1", () -> VGG(19), weights -> tvmodels.vgg19(weights=weights)),
+              ("resnet18", "IMAGENET1K_V1", () -> ResNet(18), weights -> tvmodels.resnet18(weights=weights)),
+              ("resnet34", "IMAGENET1K_V1", () -> ResNet(34), weights -> tvmodels.resnet34(weights=weights)),
+              ("resnet50", "IMAGENET1K_V1", () -> ResNet(50), weights -> tvmodels.resnet50(weights=weights)),
+              ("resnet101", "IMAGENET1K_V1", () -> ResNet(101), weights -> tvmodels.resnet101(weights=weights)),
+              ("resnet152", "IMAGENET1K_V1", () -> ResNet(152), weights -> tvmodels.resnet152(weights=weights)),
+            #   ("resnet50", "IMAGENET1K_V2", () -> ResNet(50), weights -> tvmodels.resnet50(weights=weights)),
+            #   ("resnet101", "IMAGENET1K_V2", () -> ResNet(101), weights -> tvmodels.resnet101(weights=weights)),
+            #   ("resnet152", "IMAGENET1K_V2", () -> ResNet(152), weights -> tvmodels.resnet152(weights=weights)),
+            #   ("resnext50_32x4d", "IMAGENET1K_V1", () -> ResNeXt(50, 32, 4), weights -> tvmodels.resnext50_32x4d(weights=weights)),
+            #   ("resnext101_32x8d", "IMAGENET1K_V1", () -> ResNeXt(101, 32, 8), weights -> tvmodels.resnext101_32x8d(weights=weights)),
+            #   ("wide_resnet50_2", "IMAGENET1K_V1", () -> WideResNet(50, 2), weights -> tvmodels.wide_resnet50_2(weights=weights)),
+            #   ("wide_resnet101_2", "IMAGENET1K_V1", () -> WideResNet(101, 2), weights -> tvmodels.wide_resnet101_2(weights=weights)),
+            #   ("densenet121", "IMAGENET1K_V1", () -> DenseNet(121), weights -> tvmodels.densenet121(weights=weights)),
+            #   ("squeezenet", "IMAGENET1K_V1", () -> SqueezeNet(), weights -> tvmodels.squeezenet1_0(weights=weights)),
+            ]
+
+
+# name, weights, jlconstructor, pyconstructor  = first(model_list)
+for (name, weights, jlconstructor, pyconstructor) in model_list
+    jlmodel = jlconstructor()
+    pymodel = pyconstructor(weights)
+    pytorch2flux!(jlmodel, pymodel, verb=false)
+    compare_pytorch(jlmodel, pymodel)
+    BSON.@save joinpath(@__DIR__,"$(name)_$weights.bson") model=jlmodel
+    println("Saved $(name)_$weights.bson")
+end
+
---
+++ scripts/utils.jl
@@ -0,0 +1,33 @@
+
+function imagenet_normalize(data)
+    cmean = reshape(Float32[0.485, 0.456, 0.406],(1,1,3,1))
+    cstd = reshape(Float32[0.229, 0.224, 0.225],(1,1,3,1))
+    return (data .- cmean) ./ cstd
+end
+
+
+# Julia -> Python 
+
+function jl2np(x::Array)
+    x = permutedims(x, ndims(x):-1:1)
+    x_np = Py(x).to_numpy()
+    return x_np
+end
+
+jl2th(x::Array) = torch.from_numpy(jl2np(x))
+
+# Python -> Julia
+
+function np2jl(x::Py)
+    x_jl = pyconvert(Array, x)
+    x_jl = permutedims(x_jl, ndims(x_jl):-1:1)
+    return x_jl
+end
+
+function th2jl(x::Py)
+    x_jl = pyconvert(Array, x)
+    x_jl = permutedims(x_jl, ndims(x_jl):-1:1)
+    return x_jl
+end
+
+py2jl(x::Py) = pyconvert(Any, x)
