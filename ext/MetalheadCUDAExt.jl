dc4fb0a285d20d646cd13f195c625e8ef8af5b9d
Mohamed Tarek <mohamed82008@gmail.com>
2023-10-14 03:02:10 +1100

    Metalhead. -> Metalhead.Layers.

--- ext/MetalheadCUDAExt.jl
+++ ext/MetalheadCUDAExt.jl
@@ -1,16 +1,16 @@
 module MetalheadCUDAExt
 
 if isdefined(Base, :get_extension)
-    using Metalhead: Metalhead, _dropblock_mask
+    using Metalhead: Metalhead
 else
-    using ..Metalhead: Metalhead, _dropblock_mask
+    using ..Metalhead: Metalhead
 end
 using CUDA: CUDA, CuArray
 
 ## bs is `clipped_block_size`
 # Dispatch for GPU
-Metalhead.dropblock_mask(rng::CUDA.RNG, x::CuArray, gamma, bs) = _dropblock_mask(rng, x, gamma, bs)
-function Metalhead.dropblock_mask(rng, x::CuArray, gamma, bs)
+Metalhead.Layers.dropblock_mask(rng::CUDA.RNG, x::CuArray, gamma, bs) = Metalhead.Layers._dropblock_mask(rng, x, gamma, bs)
+function Metalhead.Layers.dropblock_mask(rng, x::CuArray, gamma, bs)
     throw(ArgumentError("x isa CuArray, but rng isa $(typeof(rng)). dropblock only supports
                         CUDA.RNG for CuArrays."))
 end
