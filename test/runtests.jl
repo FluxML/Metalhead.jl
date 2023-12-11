9904b41c0856567d8285bc6d282afc1f6679ac15
Brian Chen <ToucheSir@users.noreply.github.com>
2023-12-11 15:42:46 -0800

    Fixup deps and use TestItemRunner on 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,21 @@
-using TestItems, ReTestItems
 using Metalhead: Metalhead
 
 # TODO account for GPU tests using name or tag filter
 # TODO write GPU tests!
-test_group = get(ENV, "GROUP", "All")
-name_filter = test_group == "All" ? nothing : Regex(test_group)
-ReTestItems.runtests(Metalhead; name = name_filter)
+const test_group = get(ENV, "GROUP", "All")
+const name_filter = test_group == "All" ? nothing : Regex(test_group)
+
+@static if VERSION >= v"1.7"
+    using ReTestItems
+    runtests(Metalhead; name = name_filter)
+else
+    using TestItemRunner
+    function testitem_filter(ti)
+        return name_filter === nothing || match(name_filter, ti.name) !== nothing
+    end
+end
+
+# Not sure why this needs to be split into a separate conditional...
+@static if VERSION < v"1.7"
+    @run_package_tests filter=testitem_filter
+end
