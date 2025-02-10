cd32e7391b5cf94453488f40dc8a83b26094e417
Documenter.jl <documenter@juliadocs.github.io>
2025-02-10 02:50:04 +0000

    delete history

---
+++ 0.6.0/versions.js
@@ -0,0 +1,25 @@
+var PUBLISH_ROOT = '/Metalhead.jl/0.6.0';
+var PUBLISH_VERSION = "0.6.0";
+var PUBLISH_VERSIONS = [
+    ["v0.7.3","/Metalhead.jl/v0.7.3/index.html"],
+    ["v0.7.2","/Metalhead.jl/v0.7.2/index.html"],
+    ["v0.7.1","/Metalhead.jl/v0.7.1/index.html"],
+    ["v0.7.0","/Metalhead.jl/v0.7.0/index.html"],
+    ["v0.7","/Metalhead.jl/v0.7/index.html"],
+    ["v0.6.1","/Metalhead.jl/v0.6.1/index.html"],
+    ["v0.6.0","/Metalhead.jl/v0.6.0/index.html"],
+    ["v0.6","/Metalhead.jl/v0.6/index.html"],
+    ["stable","/Metalhead.jl/stable/index.html"],
+    ["previews","/Metalhead.jl/previews/index.html"],
+    ["dev","/Metalhead.jl/dev/index.html"],
+    ["0.8.0-DEV","/Metalhead.jl/0.8.0-DEV/index.html"],
+    ["0.7.4","/Metalhead.jl/0.7.4/index.html"],
+    ["0.7.3-DEV","/Metalhead.jl/0.7.3-DEV/index.html"],
+    ["0.7.3","/Metalhead.jl/0.7.3/index.html"],
+    ["0.7.2-DEV","/Metalhead.jl/0.7.2-DEV/index.html"],
+    ["0.7.2","/Metalhead.jl/0.7.2/index.html"],
+    ["0.7.1","/Metalhead.jl/0.7.1/index.html"],
+    ["0.7.0","/Metalhead.jl/0.7.0/index.html"],
+    ["0.6.1","/Metalhead.jl/0.6.1/index.html"],
+    ["0.6.0","/Metalhead.jl/0.6.0/index.html"],
+];
